c8fc337169321c82ab3fedd603d7785182f78232
shashank artani <shashankartani@gmail.com>
2022-09-06 15:25:46 +0530

    First Commit

---
+++ LoopDemo1.java
@@ -0,0 +1,23 @@
+// Print first 10 natural numbers  (1,2,3...10)
+// Loop : 
+// for, while, do-while, for each, ....
+public class LoopDemo1 {
+	public static void main(String[] args) {
+	  int i = 1;
+	  
+	  for( ; i<=5 ; i++) {
+		  System.out.println(i);
+	  }
+	  
+	  System.out.println("Current value of i="+ i); 
+	  
+	 /* 
+	  if(true) {
+		  int x = 100;
+		  System.out.println(x);
+	  }
+	  
+	  System.out.println(x);*/
+		
+	}
+}
